4f9632a3362bb659104554152cde59baf50fea38
Gerald Sutarso <geraldsutarso2@gmail.com>
2025-03-14 17:44:45 +0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ EMOTION_LABELS = ["Neutral", "Calm", "Happy", "Sad", "Angry", "Fearful", "Disgus
 @st.cache_resource
 def load_emotion_model():
     try:
-        model = load_model("fine_tuned_emotion_model.h5")
+        model = load_model("emotion_recognition_lstm_finetune.h5")
         return model
     except Exception as e:
         st.error(f"Failed to load model: {e}")
